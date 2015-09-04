db0eb9b8d32fb24d5c389d66b1d1d56920763d65
Robinss2014 <robinsswei@gmail.com>
2015-09-03 21:00:26 -0700

    add comments in the code

--- assignment_2/test_object.rb
+++ assignment_2/test_object.rb
@@ -1,34 +1,45 @@
 # Student Name : Sisi Wei
 # Student Number : 915565877
 #
-# Please submit this file to iLearn.
+# test_object.rb
+# define class TestObject
 
 class TestObject
-  
+  # attribute reader
   attr_reader :test_fill, :test_size
 
-  def initialize(str1, str2)
-    @test_fill = str1
-    @test_size = str2
+  def initialize(fill, size)
+    # instance variables 
+    @test_fill = fill
+    @test_size = size
 
-    unless str2.is_a? Numeric
+    # when size is not a number, raise an Error
+    unless size.is_a? Numeric
     	raise ArgumentError
     end
-
   end
-
+  
+  # add a member function array
+  # return an array of size test_size, filled with test_fill
   def array
   	(1..test_size).map{ |i| "#{test_fill}" }
   end
 
+  # add a member function fill
+  # return test_fill
   def fill
   	@test_fill
   end
 
+  # add a member function size
+  # return test_size
   def size
   	@test_size
   end
 
+  # add a member function odd?
+  # return false when the test_size is even
+  # return true when the test_size is odd
   def odd?
   	@test_size % 2 != 0
   end
